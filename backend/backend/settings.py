f4fe0d78d996f4e1f09fd6880c91a8708ef933cc
Peter <Peter@unrealmail.ru>
2024-10-01 00:47:11 +0300

    cover secrets

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,8 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+DEBUG = os.getenv('DEBUG') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 # Application definition'
 
