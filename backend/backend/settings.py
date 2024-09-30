632bcbb5833ab6e7bf76c3955ace1e61c9c22835
Peter <Peter@unrealmail.ru>
2024-10-01 01:01:41 +0300

    Fix secret_key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '')
